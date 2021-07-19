d240358ee3daadf9f36488f1e0b9424575fbffaa
zzangisdaeho <zzangisdaeho@naver.com>
2021-07-19 23:37:04 +0900

    21-07-19

--- src/main/java/com/example/secondservice/controller/SecondController.java
+++ src/main/java/com/example/secondservice/controller/SecondController.java
@@ -1,13 +1,28 @@
 package com.example.secondservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SecondController {
 
-    @GetMapping("/hello")
+    @GetMapping("/welcome")
     public String hello(){
         return "second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        System.out.println(header);
+        log.info(header);
+        return "Hello in Second Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(){
+        return "second check";
     }
 }
